fdaec6458363e6d3e9a605193106b97c3747942c
nickarmenta <70037809+nickarmenta@users.noreply.github.com>
2020-09-10 15:12:50 -0500

    Got rid of sample code
    Can run class now

--- Lexium.py
+++ Lexium.py
@@ -98,25 +98,3 @@ class Lexium():
             return param_value[0]
         else:
             return None
-
-## Sample Use: Connecting, writing, and reading
-## Full list of commands can be found here (direct download link):
-## https://motion.schneider-electric.com/download/mcode-operating-system/?wpdmdl=6727&refresh=5f40386cba6e01598044268
-
-# Create class and connect
-motor = Lexium()
-motor.Connect("192.168.1.33")
-
-## Use low-leve read/write functions
-print(motor.Read('P')) # confirm motor position
-motor.Write('RC 1') # Lower run current
-motor.Write('HC 1') # Lower hold current
-
-## User higher-level intuitive functions
-motor.Accel(100)
-motor.Decel(100)
-motor.Velocity(10)
-motor.MoveBy(-10)
-motor.WaitForStop()
-motor.MoveTo(0)
-motor.WaitForStop()
